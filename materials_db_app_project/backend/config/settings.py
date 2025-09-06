ee7fb91d99357ef9594cb7343a9da0930a0c9584
NapoletanoAndrea <and.nap110@gmail.com>
2025-09-06 14:53:19 +0200

    .

--- materials_db_app_project/backend/config/settings.py
+++ materials_db_app_project/backend/config/settings.py
@@ -195,6 +195,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = get_absolute_path(os.getenv('STATIC_ROOT', '.static'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
